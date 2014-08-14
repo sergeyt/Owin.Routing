bc6ec9d63b4caff14aa5ba0b42aa9597f7e5bc32
sergeyt <stodyshev@gmail.com>
2014-08-14 22:18:47 +0700

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyCopyright("Copyright © Sergey Todyshev 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
